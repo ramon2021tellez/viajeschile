4ce941af0c38431becf5fd402b4f0cae671c5698
ramon2021tellez <101756322+ramon2021tellez@users.noreply.github.com>
2022-03-20 20:11:29 -0300

    se cambio script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,50 @@
-$(function(){
-    $("#tiramisu").on("click",function(){
- 
-    $ ("#fototiramisu , #textotiramisu").toggle(1000,function(){
-   })
-    })
-   })
-
    /*Evento jquery que genera alerta al enviar mensaje*/
         $( "#enviarcorreo" ).click(function() {
        alert("Su consulta  fue enviada correctamente!!");
        
      });
+   /*evento desaparecer contenido*/
+   $(function(){
+    $("#imagen1").on("click",function(){
+ 
+    $ ("#fotoimagen1 , #textoimagen1").toggle(1000,function(){
+   })
+    })
+   }) 
      
     /*evento desaparecer contenido*/
            $(function(){
-        $("#plateada").on("click",function(){
+        $("#imagen2").on("click",function(){
      
-        $ ("#fotoplateada , #textoplateada").toggle(1000,function(){
+        $ ("#fotoimagen2 , #textoimagen2").toggle(1000,function(){
        })
         })
        })
     /*-evento desaparecer contenido*/
                 $(function(){
-          $("#panqueques").on("click",function(){
+          $("#imagen3").on("click",function(){
      
-          $ ("#fotopanqueque , #textopanqueque").toggle(1000,function(){
+          $ ("#fotoimagen3 , #textoimagen3").toggle(1000,function(){
          })
           })
          })
-   
-   /*efecto cambio color fuente doble click ingredientes*/
-   $( "#ingredientes,#preparacion" ).dblclick(function() {
-   $(this).css({
-      "color":"red"
-        })
-    });  
-    smoothScroll.init({
-    selector: ['data-scroll'],
-    selectorheader: null,
-    speed:2000,
-   easing:'easeInOutCubic',
-   offset:0,
-   callback:function(ancho,toggle){}
+/*-evento desaparecer contenido*/
+$(function(){
+  $("#imagen4").on("click",function(){
+
+  $ ("#fotoimagen4 , #textoimagen4").toggle(1000,function(){
+ })
+  })
+ })
+
+ smoothScroll.init({
+      selector: ['data-scroll'],
+      selectorheader: null,
+      speed:2000,
+      easing:'easeInOutCubic',
+      offset:0,
+      callback:function(ancho,toggle){}
  }); 
  $(function () {
-   $('[data-toggle="tooltip"]').tooltip()
+      $('[data-toggle="tooltip"]').tooltip()
  })
